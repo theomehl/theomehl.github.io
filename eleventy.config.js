b320b84f57a23f14a39c7dfe3d8f75ef8fc3cabb
Ted Mehl <ted.mehl@gmail.com>
2024-11-12 22:52:30 -0600

    console.log the parsed dates

--- eleventy.config.js
+++ eleventy.config.js
@@ -21,6 +21,7 @@ export default function(eleventyConfig) {
     eleventyConfig.addPreprocessor("drafts", "*", (data) => {
       // Account for central time adjustment in date comparison. Otherwise, dates act as midnight UTC which is 6 p.m. CT the previous day.
       let adjustedDate = Date.parse(data.page.date) + 18000000;
+      console.log(`${data.title}: ${adjustedDate} vs ${Date.now()}`);
       if((adjustedDate > Date.now() && process.env.ELEVENTY_RUN_MODE === "build")) {
         return false;
       }
