5ccca06b25d98971249147324f532c7ffd34d83a
Ted Mehl <ted.mehl@gmail.com>
2024-11-12 22:42:54 -0600

    Remove draft preprocessor for testing purposes

--- eleventy.config.js
+++ eleventy.config.js
@@ -18,13 +18,13 @@ export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./CNAME');
 
     // Only process pages that don't have a draft variable set to true or are after today's date.
-    eleventyConfig.addPreprocessor("drafts", "*", (data) => {
-      // Account for central time adjustment in date comparison. Otherwise, dates act as midnight UTC which is 6 p.m. CT the previous day.
-      let adjustedDate = Date.parse(data.page.date) + 18000000;
-      if((data.draft && process.env.ELEVENTY_RUN_MODE === "build") || (adjustedDate > Date.now() && process.env.ELEVENTY_RUN_MODE === "build")) {
-        return false;
-      }
-    });
+    // eleventyConfig.addPreprocessor("drafts", "*", (data) => {
+    //   // Account for central time adjustment in date comparison. Otherwise, dates act as midnight UTC which is 6 p.m. CT the previous day.
+    //   let adjustedDate = Date.parse(data.page.date) + 18000000;
+    //   if((data.draft && process.env.ELEVENTY_RUN_MODE === "build") || (adjustedDate > Date.now() && process.env.ELEVENTY_RUN_MODE === "build")) {
+    //     return false;
+    //   }
+    // });
 
     return {
       dir: {
