fc05367178ffe99f0c040c86907992a07df8ee5f
Ted Mehl <ted.mehl@gmail.com>
2025-06-27 16:08:52 -0500

    Add adblock detection demo

---
+++ src/demos/files/floatingad.js
@@ -0,0 +1,7 @@
+console.log('loading banner ad...');
+let container = document.querySelector('#ad-container');
+let element = document.createElement('div');
+element.id = 'Adbanner';
+element.style.display='none';
+
+container.appendChild(element);
