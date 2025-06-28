65c8c80aa5f3411d3f9379d708624eb878e431bc
theomehl <theomehl@users.noreply.github.com>
2025-06-28 06:13:04 +0000

    deploy: 27ca98efe58005a2903345059dc17d4b70960cb6

--- demos/files/nativeads.js
+++ demos/files/nativeads.js
@@ -2,6 +2,7 @@ console.log('loading banner ad...');
 let container = document.querySelector('#ad-container');
 let element = document.createElement('div');
 element.id = 'Adbanner';
-element.style.display='none';
+element.innerHTML = 'On a website running real ads, this would have been a banner ad.';
+element.classList.add('notice');
 
 container.appendChild(element);
