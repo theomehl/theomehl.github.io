1febea2afc509debeb1a23d5684045b286d17247
Ted Mehl <ted.mehl@gmail.com>
2026-02-14 19:52:48 -0600

    Use git last modified on prod builds for commonplace updated date

--- src/commonplace/commonplace.11tydata.js
+++ src/commonplace/commonplace.11tydata.js
@@ -1,7 +1,13 @@
-export default {
+let data = {
     tags: "commonplace",
     layout: "layouts/commonplace-post.njk",
     permalink: function({page}) {
         return `/commonplace/${page.fileSlug}/`;
     }
 }
+
+if (process.env.ELEVENTY_RUN_MODE === "build") {
+    data.date = "git Last Modified";
+}
+
+export default data;
